3b4e4a29361a3ec6c3a191d26c3fac140f9f03d9
simon <simong@GUILBAUD.SG>
2025-03-11 14:28:09 +0100

    sycro

---
+++ OU_GUILBAUD.SG.ps1
@@ -0,0 +1,59 @@
+﻿# Importer le module Active Directory
+Import-Module ActiveDirectory
+
+# Chemin vers le fichier CSV
+$csvPath = "OU_GUILBAUD.SG.csv"
+
+# Importer les données du fichier CSV
+$OUs = Import-Csv -Path $csvPath
+
+# Créer l'OU principale si elle n'existe pas
+$mainOUName = "_GUILBAUD.SG"
+$mainOUPath = "DC=GUILBAUD,DC=SG"
+
+if (-not (Get-ADOrganizationalUnit -Filter { Name -eq $mainOUName } -ErrorAction SilentlyContinue)) {
+    New-ADOrganizationalUnit -Name $mainOUName -Path $mainOUPath
+    Write-Host "OU principale '$mainOUName' créée."
+} else {
+    Write-Host "L'OU principale '$mainOUName' existe déjà."
+}
+
+# Créer un dictionnaire pour garder une trace des OUs créées
+$createdOUs = @{}
+
+# Créer les OUs en s'assurant que les parents existent
+foreach ($OU in $OUs) {
+    $parentOU = $OU.ParentOU
+    $parentOUPath = $null
+
+    if ($parentOU -ne "") {
+        # Vérifier si l'OU parent a été créée
+        if ($createdOUs.ContainsKey($parentOU)) {
+            $parentOUPath = $createdOUs[$parentOU]
+        } else {
+            # Récupérer le chemin de l'OU parent
+            $parentOUObj = Get-ADOrganizationalUnit -Filter { Name -eq $parentOU } -ErrorAction SilentlyContinue
+            if ($parentOUObj) {
+                $parentOUPath = $parentOUObj.DistinguishedName
+            }
+        }
+
+        if ($parentOUPath) {
+            try {
+                # Créer l'OU si le parent existe
+                New-ADOrganizationalUnit -Name $OU.OUName -Path $parentOUPath
+                $createdOUs[$OU.OUName] = "OU=$($OU.OUName),$parentOUPath"
+                Write-Host "OU '$($OU.OUName)' créée sous '$parentOU'."
+            } catch {
+                Write-Host "Erreur lors de la création de l'OU '$($OU.OUName)': $_"
+            }
+        } else {
+            Write-Host "L'OU parent '$parentOU' n'existe pas. Impossible de créer '$($OU.OUName)'."
+        }
+    } else {
+        # Cas où l'OU n'a pas de parent (par exemple, la principale)
+        Write-Host "L'OU '$($OU.OUName)' n'a pas de parent défini."
+    }
+}
+
+Write-Host "Création des unités d'organisation terminée."
---
+++ Users_GUILBAUD.SG.ps1
@@ -0,0 +1,54 @@
+﻿# Import active directory 
+Import-Module activedirectory
+$Infosdomaine = Get-ADDomain
+$DN = $infosdomaine.DistinguishedName
+$DNSRoot = $Infosdomaine.DNSRoot
+
+
+#Importer les utilisateur du fichier csv dans la variable $ADUsers 
+$ADUsers = Import-csv .\Users_GUILBAUD.SG.csv -Delimiter ";" 
+
+#On va variabiliser chaque colonne du fichier csv 
+foreach ($User in $ADUsers)
+{
+			
+	$Username = $User.EmployeeID
+	#$Password = $User.password
+	$Firstname = $User.firstname
+	$Lastname = $User.lastname
+	$OU = $User.OU
+    $Dpt = $User.Department
+    $Function = $User.Department
+    #$Compagny = $User.company
+    $samaccountname = $user.EmployeeID
+    $Des = $user.Description
+    $Titlle = $user.Title
+    $fullname = $user."$Firstname" + "$Lastname"
+
+if (Get-ADUser -F {SamAccountName -eq $Username})
+	{
+		 #If user does exist, give a warning
+		 Write-Warning "A user account with username $Username already exist in Active Directory."
+	}
+	else
+	{
+		#Si l'utilisateur n'existe pas ou va le créer
+		
+        #On va récupérer les information dans les variables créées ci-dessus
+        New-ADUser -Name "$fullname" -SamAccountName "$Username" -Surname "$Lastname" -GivenName "$Firstname" -AccountPassword (convertto-securestring "Passw0rd" -AsPlainText -Force) -UserPrincipalName "$Username@$DNSRoot" -Department "$Dpt" -Title "$Titlle" -Path "$OU" -Enabled $true -ChangePasswordAtLogon $false -Description "$Des" -EmailAddress "$Username@$DNSRoot" -Instance $Function -DisplayName "$fullname" 
+		
+                    
+}
+}
+
+$userdis = (Get-ADUser -Filter * -Properties * | select name,department,samaccountname | where {$_.department -Like "Du-Mal" -or $_.department -like "Maléfiques"} )
+[System.Collections.ArrayList]$test = ($userdis | select samaccountname)
+
+foreach ($line in $test)
+{
+			
+	$Useradis= $line.samaccountname
+
+Set-ADUser -Identity "$Useradis" -Enabled $false
+}
+
