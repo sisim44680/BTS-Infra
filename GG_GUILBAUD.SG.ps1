f6281d4d39a6e540dde5f6a2e99edd030537007e
simon <simong@GUILBAUD.SG>
2025-03-11 14:37:03 +0100

    sycro

---
+++ GG_GUILBAUD.SG.ps1
@@ -0,0 +1,25 @@
+﻿# Import active directory 
+Import-Module activedirectory
+$Infosdomaine = Get-ADDomain$DN = $infosdomaine.DistinguishedName
+$DNSRoot = $Infosdomaine.DNSRoot
+
+
+$ADgrps = Import-csv .\GRP-DL.csv -Delimiter ";" 
+
+foreach ($grp in $ADgrps)
+{
+$OU = $grp.OU
+$grpgg = $grp.groupe
+$desgrp = $grp.description
+
+try
+{
+Get-ADGroup -Identity $grpgg -ErrorAction Ignore | ft name -HideTableHeaders
+Write-Warning "Le groupe $grpgg existe déjà" 
+}
+catch
+{
+New-ADGroup -Name "$grpgg" -GroupScope global -GroupCategory Security -Path "$OU" -Description "$desgrp" -ErrorAction SilentlyContinue
+Write-Host "Le groupe $grpgg a bien été crée"
+}
+}
